c6c484ebc64554d6c9dafb5a356c83f0f0296237
hare <washi79washi@gmail.com>
2022-08-23 22:00:24 +0900

    “blog”

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+  // const pagelink = document.querySelectorAll('.pagination li');
+
+  // pagelink.addEventListener('click', () => {
+  //     del();
+  //     pagelink.forEach(entry => {
+  //       entry.classList.add('current');
+  //     })
+  //       // for(i < pagelink.length i++ i != entry) {
+  //       //   pagelink[i].classList.remove('current');
+  //       // }
+
+  //   });
+  // function del() {
+  //   pagelink.forEach(entry => {
+  //     entry.classList.remove('current');
+  //   });
+  // }
+}
